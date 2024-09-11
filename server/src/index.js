4e02a0467354d09cd43af54bd54bef5a57c7a1f1
sundeeep <sandeepkumar25820@gmail.com>
2024-09-12 03:41:34 +0530

    Socket IO Server SetUp Done!

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,20 @@ import http from "http";
 import dotenv from "dotenv";
 import { PORT } from "./constants.js";
 dotenv.config({path: "./.env"});
+import {Server as SocketServer} from "socket.io";
+import cors from "cors";
+import os from "os";
+
 
 const app = express();
 
 const httpServer = http.createServer(app);
+
+const socketIO = new SocketServer({
+    cors: '*'
+})
+
+socketIO.attach(httpServer);
 
 httpServer.listen(PORT, ()=>{
     console.log("Bridge Server is running on port ", PORT);
